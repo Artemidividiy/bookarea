63cd19bd9fc9dcfe1c966007a8ea79177821aa8a
notblank00 <pyromancy00@gmail.com>
2021-12-28 09:39:01 +0300

    fixed mtm validation error

--- bookarea/app/models/user.rb
+++ bookarea/app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_many :relations
   has_many :books, through: :relations
   has_secure_password
-  validates_presence_of :username, :email, :password
+  validates_presence_of :username, :email, :password_digest
   validates_uniqueness_of :username, :email
-   validates :password, confirmation: { case_sensitive: true }
-  end
-  
+  validates :password, confirmation: { case_sensitive: true }
+end
