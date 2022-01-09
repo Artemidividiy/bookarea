3c63cd7d286705609eed6a037b5808b37600fe32
notblank00 <pyromancy00@gmail.com>
2022-01-09 18:50:35 +0300

    null safety

--- bookarea/spec/views/books/index.html.erb_spec.rb
+++ bookarea/spec/views/books/index.html.erb_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "books/index", type: :view do
+RSpec.describe 'books/index', type: :view do
   before(:each) do
     assign(:books, [
-      Book.create!(
-        name: "Name",
-        author: "Author",
-        publishing_house: "Publishing House",
-        year: 2,
-        isbn: "Isbn"
-      ),
-      Book.create!(
-        name: "Name",
-        author: "Author",
-        publishing_house: "Publishing House",
-        year: 2,
-        isbn: "Isbn2"
-      )
-    ])
+             Book.create!(
+               name: 'Name',
+               author: 'Author',
+               publishing_house: 'Publishing House',
+               year: 2,
+               isbn: 'Isbn'
+             ),
+             Book.create!(
+               name: 'Name',
+               author: 'Author',
+               publishing_house: 'Publishing House',
+               year: 2,
+               isbn: 'Isbn2'
+             )
+           ])
   end
 
-  it "renders a list of books" do
+  it 'renders a list of books' do
     render
-    expect(rendered).to include("Isbn", "Isbn2")
+    expect(rendered).to include('Isbn', 'Isbn2')
   end
 end
