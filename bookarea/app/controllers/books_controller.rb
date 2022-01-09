4049f49b27f777a5366fea7ed2aa4154573d63c8
notblank00 <pyromancy00@gmail.com>
2022-01-09 19:24:26 +0300

    added ajax on like/unlike button

--- bookarea/app/controllers/books_controller.rb
+++ bookarea/app/controllers/books_controller.rb
@@ -1,15 +1,14 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show edit update destroy like unlike]
-  def input;end
-  
+  before_action :set_book, only: %i[show edit update destroy like unlike]
+  def input; end
+
   # GET /books or /books.json
   def index
     @books = Book.all
   end
 
   # GET /books/1 or /books/1.json
-  def show
-  end
+  def show; end
 
   # GET /books/new
   def new
@@ -17,17 +16,15 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit
-  end
+  def edit; end
 
   def unlike
     current_user.books.delete @book
-    
   end
-  def like 
+
+  def like
     current_user.books << @book
     # Relation.create(user_id: current_user.id, book_id: @book.id)
-    puts 'hueta'
   end
 
   # POST /books or /books.json
@@ -36,7 +33,7 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         current_user.books << @book
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.html { redirect_to book_url(@book), notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -49,7 +46,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.html { redirect_to book_url(@book), notice: 'Book was successfully updated.' }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -63,19 +60,20 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def book_params
-      params.require(:book).permit(:name, :author, :publishing_house, :year, :isbn)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:name, :author, :publishing_house, :year, :isbn)
+  end
 end
